bc9fe88e3911e520639f66d1096cbf4f915862f3
isak yıldırım <isakyildirim@outlook.com>
2022-05-29 18:20:51 +0300

    Kullanıcı profil sayfası yapıldı

---
+++ Pet.AspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pet.AspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
