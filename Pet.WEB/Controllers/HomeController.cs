526198fa6cf3e983a6fdc90def2a00cd09cf813a
isak yıldırım <isakyildirim@outlook.com>
2022-06-15 00:02:14 +0300

    İl seçme denedi ama olmadı

--- Pet.WEB/Controllers/HomeController.cs
+++ Pet.WEB/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pet.Entities.Context;
 using Pet.WEB.Models;
 using System.Diagnostics;
+using System.Web.Mvc;
+using Controller = Microsoft.AspNetCore.Mvc.Controller;
+using JsonResult = Microsoft.AspNetCore.Mvc.JsonResult;
 
 namespace Pet.WEB.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        SqlDbContext sqlDb;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            sqlDb = new SqlDbContext();
         }
 
         public IActionResult Index()
@@ -28,5 +33,18 @@ namespace Pet.WEB.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        //public JsonResult IlceGetir(int p)
+        //{
+        //    var ilceler = (from x in sqlDb.Districts
+        //                   join y in sqlDb.Cities on x.City.Id equals y.Id
+        //                   where x.City.Id == p
+        //                   select new
+        //                   {
+        //                       Text = x.DistrictName,
+        //                       Value = x.Id.ToString()
+        //                   }).ToList();
+        //    return Json(ilceler,JsonRequestBehavior.AllowGet);
+        //}
     }
 }
