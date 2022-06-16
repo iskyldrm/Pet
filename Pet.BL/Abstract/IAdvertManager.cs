6f794c8515a60902e171f688efcdfdc3fea5dc28
isak yıldırım <isakyildirim@outlook.com>
2022-06-16 23:28:11 +0300

    BL katmanı Manager sınıfları oluşturuldu

---
+++ Pet.BL/Abstract/IAdvertManager.cs
@@ -0,0 +1,13 @@
+﻿using Pet.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet.BL.Abstract
+{
+    public interface IAdvertManager:IManagerBase<Advert>
+    {
+    }
+}
---
+++ Pet.BL/Concrete/KindManager.cs
@@ -0,0 +1,14 @@
+﻿using Pet.BL.Abstract;
+using Pet.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet.BL.Concrete
+{
+    public class KindManager:ManagerBase<Kind>,IKindManager
+    {
+    }
+}
