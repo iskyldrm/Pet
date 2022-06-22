b94c675d71a58836be1f302b2ce93f7bca595c8c
isak yıldırım <isakyildirim@outlook.com>
2022-06-22 18:02:21 +0300

    PET ADDED CS OLUŞTURULDU

--- Pet.WEB/Areas/Pet/Pages/AddPet/AddPetIndex.cshtml.cs
+++ Pet.WEB/Areas/Pet/Pages/AddPet/AddPetIndex.cshtml.cs
@@ -1,12 +1,68 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pet.Entities.Concrete;
 
 namespace Pet.WEB.Areas.Pet.Pages.AddPet
 {
     public class AddPetIndexModel : PageModel
     {
-        public void OnGet()
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AddPetIndexModel> _logger;
+
+        public AddPetIndexModel(SignInManager<User> signInManager, ILogger<AddPetIndexModel> logger)
         {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string ReturnUrl { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            public string LivingName { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            // If we got this far, something failed, redisplay form
+            return Page();
         }
     }
 }
